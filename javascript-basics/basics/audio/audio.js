1b0c2334212c195512de34d6399c4a24bf86cd7d
Felippe Santana <felippemenezes1982@gmail.com>
2026-03-25 15:21:04 -0300

    JavaScript audio stream

---
+++ javascript-basics/basics/audio/audio.js
@@ -0,0 +1,9 @@
+const audioElement = document.querySelector("audio");
+navigator.mediaDevices
+  .getUserMedia({ audio: true })
+  .then((stream) => {
+    audioElement.srcObject = stream;
+  })
+  .catch((error) => {
+    console.error("Error accessing the microphone", error);
+  });
