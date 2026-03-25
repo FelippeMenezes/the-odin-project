e8b6f3c20742e7be4ea4249b42b533e7f3451dec
Felippe Santana <felippemenezes1982@gmail.com>
2026-03-25 17:44:07 -0300

    If, else if and else ecercise

---
+++ javascript/condicional-and-operators/index.js
@@ -0,0 +1,9 @@
+let userNumber = prompt("Please, enter a number that will be displayed in an alert.");
+
+if (userNumber > 0) {
+    alert(1)
+} else if (userNumber < 0) {
+    alert(-1)
+} else {
+    alert(0)
+}
