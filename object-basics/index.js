f96860be2d756af0ed9d96c84a4b740255ba6ef8
Felippe Santana <felippemenezes1982@gmail.com>
2026-04-03 10:59:19 -0300

    Complete map to names exercise

--- object-basics/index.js
+++ object-basics/index.js
@@ -29,3 +29,14 @@ let primitive = 0;
 
 increaseCounterObject(object);
 increaseCounterPrimitive(primitive);
+
+let data = 42;
+// dataCopy will store a copy of what data contains, so a copy of 42
+let dataCopy = data;
+
+// which means that making changes to dataCopy won't affect data
+dataCopy = 43;
+
+console.log(data); // 42
+console.log(dataCopy); // 43
+
