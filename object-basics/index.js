1422660ba2357d2335609d7a81c21dffa6e2d71c
Felippe Santana <felippemenezes1982@gmail.com>
2026-04-03 10:13:03 -0300

    Introduce to js objects

--- object-basics/index.js
+++ object-basics/index.js
@@ -15,3 +15,17 @@ const person = {
 const myDataName = "height";
 const myDataValue = "1.75m";
 person[myDataName] = myDataValue;
+
+function increaseCounterObject(objectCounter) {
+  objectCounter.counter += 1;
+}
+
+function increaseCounterPrimitive(primitiveCounter) {
+  primitiveCounter += 1;
+}
+
+const object = { counter: 0 };
+let primitive = 0;
+
+increaseCounterObject(object);
+increaseCounterPrimitive(primitive);
