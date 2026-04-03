9d605a04406c5227c648e0d5e3fbd368744c6b11
Felippe Santana <felippemenezes1982@gmail.com>
2026-04-03 16:47:35 -0300

    Compelete Get average age exercise

--- javascript/array-methods/get-average-age/get-average-age.js
+++ javascript/array-methods/get-average-age/get-average-age.js
@@ -4,5 +4,13 @@ let mary = { name: "Mary", age: 29 };
 
 let arr = [ john, pete, mary ];
 
+function getAverageAge(users) {
+    let sum = 0;
+    for (let i = 0; i < users.length; i++) {
+        sum += users[i].age;
+    }
+    return sum / users.length;
+};
+
 alert( getAverageAge(arr) ); // (25 + 30 + 29) / 3 = 28
 
