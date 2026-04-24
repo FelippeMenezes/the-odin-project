5241757ed8b5e638c686b77f6f114d10c4ef4894
FelippeMenezes <42714571+FelippeMenezes@users.noreply.github.com>
2026-04-24 11:13:28 -0300

    Exercise 7

--- full-stack-ruby-on-rails/ruby/basic_ruby/nested_hashes/exercise_7.rb
+++ full-stack-ruby-on-rails/ruby/basic_ruby/nested_hashes/exercise_7.rb
@@ -1,94 +1,94 @@
-# numeros = [1, 2, 3, 4, 5]
+numeros = [1, 2, 3, 4, 5]
 
-# triplo_numeros = numeros.map { |numero| numero * 3}
-# p triplo_numeros
+triplo_numeros = numeros.map { |numero| numero * 3}
+p triplo_numeros
 
 
-# nomes = ["felippe", "ana", "bruno", "carla"]
+nomes = ["felippe", "ana", "bruno", "carla"]
 
-# nomes_maiusculos = nomes.map { |nome| nome.capitalize}
-# p nomes_maiusculos
+nomes_maiusculos = nomes.map { |nome| nome.capitalize}
+p nomes_maiusculos
 
-# produtos = [
-#   { nome: "Teclado", preco: 250 },
-#   { nome: "Mouse",   preco: 80  },
-#   { nome: "Monitor", preco: 1200 }
-# ]
+produtos = [
+  { nome: "Teclado", preco: 250 },
+  { nome: "Mouse",   preco: 80  },
+  { nome: "Monitor", preco: 1200 }
+]
 
-# nomes_produtos = produtos.map { |produto| produto[:nome]}
-# p nomes_produtos
+nomes_produtos = produtos.map { |produto| produto[:nome]}
+p nomes_produtos
 
-# produtos_precificados = [
-#   { nome: "Teclado", preco: 250 },
-#   { nome: "Mouse",   preco: 80  },
-#   { nome: "Monitor", preco: 1200 }
-# ]
+produtos_precificados = [
+  { nome: "Teclado", preco: 250 },
+  { nome: "Mouse",   preco: 80  },
+  { nome: "Monitor", preco: 1200 }
+]
 
-# produtos_desconto_10 = produtos_precificados.map do |item|
-#   {
-#     nome: item[:nome],
-#     preco: (item[:preco] * 90) / 100
-#   }
-# end
+produtos_desconto_10 = produtos_precificados.map do |item|
+  {
+    nome: item[:nome],
+    preco: (item[:preco] * 90) / 100
+  }
+end
 
-# p produtos_desconto_10
+p produtos_desconto_10
 
-# pedidos = [
-#   { produto: "Pizza",   valor: 45.0 },
-#   { produto: "Coca",    valor: 8.0  },
-#   { produto: "Sorvete", valor: 18.0 }
-# ]
+pedidos = [
+  { produto: "Pizza",   valor: 45.0 },
+  { produto: "Coca",    valor: 8.0  },
+  { produto: "Sorvete", valor: 18.0 }
+]
 
-# pedidos_formatados = pedidos.map { |produto| "#{produto[:produto]}: R$ #{produto[:valor]}"}
-# p pedidos_formatados
+pedidos_formatados = pedidos.map { |produto| "#{produto[:produto]}: R$ #{produto[:valor]}"}
+p pedidos_formatados
 
-# funcionarios = [
-#   { nome: "Ana",    salario: 3000 },
-#   { nome: "Bruno",  salario: 8000 },
-#   { nome: "Carla",  salario: 5000 },
-#   { nome: "Diego",  salario: 12000 }
-# ]
+funcionarios = [
+  { nome: "Ana",    salario: 3000 },
+  { nome: "Bruno",  salario: 8000 },
+  { nome: "Carla",  salario: 5000 },
+  { nome: "Diego",  salario: 12000 }
+]
 
-# nivel_funcionarios = funcionarios.map do |funcionario|
-#   if funcionario[:salario] <= 5000
-#     funcionario.merge(nivel: :junior)
-#   elsif funcionario[:salario] > 5000 && funcionario[:salario] <= 9000
-#     funcionario.merge(nivel: :pleno)
-#   else
-#     funcionario.merge(nivel: :senior)
-#   end
-# end
-# p nivel_funcionarios
+nivel_funcionarios = funcionarios.map do |funcionario|
+  if funcionario[:salario] <= 5000
+    funcionario.merge(nivel: :junior)
+  elsif funcionario[:salario] > 5000 && funcionario[:salario] <= 9000
+    funcionario.merge(nivel: :pleno)
+  else
+    funcionario.merge(nivel: :senior)
+  end
+end
+p nivel_funcionarios
 
-# turmas = [
-#   { nome: "Turma A", alunos: ["felippe", "ana", "bruno"] },
-#   { nome: "Turma B", alunos: ["carla", "pedro"] }
-# ]
+turmas = [
+  { nome: "Turma A", alunos: ["felippe", "ana", "bruno"] },
+  { nome: "Turma B", alunos: ["carla", "pedro"] }
+]
 
-# alunos_maisculos = turmas.map do |turma|
-#   {
-#   nome: turma[:nome],
-#   alunos: turma[:alunos].map do |aluno|
-#     aluno.capitalize
-#   end
-#   }
-# end
+alunos_maisculos = turmas.map do |turma|
+  {
+  nome: turma[:nome],
+  alunos: turma[:alunos].map do |aluno|
+    aluno.capitalize
+  end
+  }
+end
 
-# p alunos_maisculos
+p alunos_maisculos
 
-# estoque = [
-#   { nome: "Notebook", preco: 3500.0, disponivel: true  },
-#   { nome: "Tablet",   preco: 1200.0, disponivel: false },
-#   { nome: "Teclado",  preco: 250.0,  disponivel: true  },
-#   { nome: "Mouse",    preco: 80.0,   disponivel: false },
-#   { nome: "Monitor",  preco: 1200.0, disponivel: true  }
-# ]
+estoque = [
+  { nome: "Notebook", preco: 3500.0, disponivel: true  },
+  { nome: "Tablet",   preco: 1200.0, disponivel: false },
+  { nome: "Teclado",  preco: 250.0,  disponivel: true  },
+  { nome: "Mouse",    preco: 80.0,   disponivel: false },
+  { nome: "Monitor",  preco: 1200.0, disponivel: true  }
+]
 
-# produtos_disponiveis = estoque
-#   .select { |item| item[:disponivel] }
-#   .map { |item| item[:nome] }
+produtos_disponiveis = estoque
+  .select { |item| item[:disponivel] }
+  .map { |item| item[:nome] }
 
-# p produtos_disponiveis
+p produtos_disponiveis
 
 times = [
   { nome: "Backend",  membros: ["Felippe", "Ana"],           lider: "Felippe" },
@@ -102,3 +102,37 @@ strings_formatadas = times.map do |time|
 end
 
 p strings_formatadas
+
+relatorio = [
+  {
+    departamento: "Tech",
+    funcionarios: [
+      { nome: "Felippe", salario: 8000, horas_extras: 10 },
+      { nome: "Ana",     salario: 6000, horas_extras: 0  },
+      { nome: "Bruno",   salario: 9500, horas_extras: 5  }
+    ]
+  },
+  {
+    departamento: "Design",
+    funcionarios: [
+      { nome: "Carla", salario: 7000, horas_extras: 8 },
+      { nome: "Diego", salario: 5500, horas_extras: 0 }
+    ]
+  }
+]
+
+relatorio_final = relatorio.map do |item|
+  {
+    departamento: item[:departamento],
+    funcionarios: item[:funcionarios].map do |funcionario|
+      {
+        nome: funcionario[:nome],
+        salario: funcionario[:salario],
+        horas_extras: funcionario[:horas_extras],
+        salario_final: (funcionario[:salario] + (funcionario[:horas_extras] * 50))
+      }
+    end
+  }
+end
+
+p relatorio_final
