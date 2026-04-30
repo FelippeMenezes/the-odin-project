cf1ae6d9fbae665a9c93d153928a887e381d45c0
FelippeMenezes <42714571+FelippeMenezes@users.noreply.github.com>
2026-04-30 20:45:03 -0300

    Pattern matching

---
+++ full-stack-ruby-on-rails/ruby/advanced_ruby/pattern_matching/Introduction.rb
@@ -0,0 +1,17 @@
+# grade = 'C'
+
+# case grade
+# when 'A' then puts 'Amazing effort'
+# when 'B' then puts 'Good work'
+# when 'C' then puts 'Well done'
+# when 'D' then puts 'Room for improvement'
+# else puts 'See me'
+# end
+
+
+a = 5
+
+case 1
+in ^a
+  puts "match"
+end
