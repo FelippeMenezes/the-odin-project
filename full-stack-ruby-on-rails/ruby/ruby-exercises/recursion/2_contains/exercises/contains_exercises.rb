1522385b824963ee854876172a71d999b8b6f26e
FelippeMenezes <42714571+FelippeMenezes@users.noreply.github.com>
2026-05-13 14:45:27 -0300

    Contains exercise

--- full-stack-ruby-on-rails/ruby/ruby-exercises/recursion/2_contains/exercises/contains_exercises.rb
+++ full-stack-ruby-on-rails/ruby/ruby-exercises/recursion/2_contains/exercises/contains_exercises.rb
@@ -5,4 +5,10 @@ def contains?(hash, search_value)
   # Examples:
   # contains?({ foo: { bar: "baz" } }, "baz") # true
   # contains?({ foo: { bar: "baz" } }, "egg") # false
+
+  hash_values = hash.values
+  return true if hash_values.include?(search_value)
+
+  nested_hashes = hash_values.select { |value| value.is_a?(Hash) }
+  nested_hashes.any? { |nested_hash| contains?(nested_hash, search_value) }
 end
