c0964fd52dc21b20e4b2e3f954c32d23a8d62aab
FelippeMenezes <42714571+FelippeMenezes@users.noreply.github.com>
2026-05-18 10:16:19 -0300

    Tally exercise

---
+++ full-stack-ruby-on-rails/ruby/basic_ruby/arrays/exercise3.rb
@@ -0,0 +1,14 @@
+# frutas = ["maçã", "banana", "maçã", "laranja", "banana", "maçã"]
+
+# count_fruits_hash = frutas.tally
+
+# p count_fruits_hash
+
+pedidos = [:pendente, :pago, :cancelado, :pago, :pendente,
+           :pago, :entregue, :cancelado, :pago, :pendente]
+
+count_status_orders = pedidos.tally
+
+p count_status_orders
+
+
