065a5df12cc14bdf39f9f26c2a84a20330927438
FelippeMenezes <42714571+FelippeMenezes@users.noreply.github.com>
2026-05-26 21:58:15 -0300

    Exercise 2

---
+++ full-stack-ruby-on-rails/ruby/basic_ruby/methods/tally.rb
@@ -0,0 +1,9 @@
+# palavras = ["ruby", "python", "ruby", "java", "python", "ruby"]
+
+# p frequencia_palavra = palavras.tally
+
+# respostas = ["sim", "Sim", "NÃO", "não", "sim", "NÃO", "Sim"]
+
+# p frequencia_respostas = respostas.map { |palavra| palavra.downcase }.tally
+
+
